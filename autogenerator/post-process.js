c40c4ff9ebb713521995ccc2d0c405569a7ea8fd
Gianfranco Tasteri <gianfra33@gmail.com>
2023-10-02 14:38:15 -0300

    add post process script to solve the missing constant issue

---
+++ autogenerator/post-process.js
@@ -0,0 +1,25 @@
+//this script will read any definition of the form xdr.const("CONST_NAME", VALUE), 
+//and define it on top of the .ts file for other types to use.
+
+const fs = require('fs');
+const path = require('path');
+
+
+const filePath = path.join(__dirname, 'x2JavaScript/generated/stellar-xdr_generated.ts');
+const content = fs.readFileSync(filePath, 'utf8');
+
+const regex = /xdr\.const\("([A-Za-z0-9_]+)",\s*(\d+|"[^"]*")\);/g;
+let match;
+let constDefinitions = '';
+
+while ((match = regex.exec(content)) !== null) {
+    const [fullMatch, constName, constValue] = match;
+    constDefinitions += `const ${constName} = ${constValue};\n`;
+}
+
+const newContent = constDefinitions + '\n' + content;
+
+
+const newFilePath = path.join(__dirname, 'x2JavaScript/generated/stellar-xdr_generated.ts');
+fs.writeFileSync(newFilePath, newContent);
+
