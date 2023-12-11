f70a1d78addc5c7de5d527007897209a0f142eb6
Gianfranco Tasteri <gianfra33@gmail.com>
2023-12-11 19:04:58 -0300

    upgrade fixes

--- src/xdr/impls/error.rs
+++ src/xdr/impls/error.rs
@@ -1,9 +1,10 @@
 use crate::{lib::String, types::Error};
+use scale_info::prelude::format;
 
 impl<E: From<sp_std::str::Utf8Error>> crate::StellarTypeToString<Self, E> for Error {
     fn as_encoded_string(&self) -> Result<String, E> {
         let msg = self.msg.get_vec();
-        let msg = sp_std::str::from_utf8(msg).map_err(E::from)?.to_string();
+        let msg = sp_std::str::from_utf8(msg).map_err(E::from)?;
         Ok(format!("Error{{ code:{:?} message:{msg} }}", self.code))
     }
 }
